eeaa10a9be757e76f9f72ae006c8028b36d4ed78
NoopurShinde <noopur.shinde@harbingergroup.com>
2020-01-03 17:02:56 +0530

    Completed routing. Need to check edit flow

---
+++ exampledemo/.history/src/App_20200103150200.js
@@ -0,0 +1,156 @@
+import React, { Component } from 'react'
+
+import Table from './Table'
+import Form from "./form"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      editing: false,
+      characters:[],
+      username:'',
+      isPresent:false,
+    }
+  } 
+  showValidationError(element, msg){
+    this.setState((prevState) => ({
+      errors: [...prevState.errors, {element, msg}]
+    }))
+  }
+  removedata = index => {
+    const {characters} = this.state
+    this.setState({
+      characters: characters.filter((characters, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  editdata = (index, value) => {
+    console.log("vlaues t be updated", value)
+    // this.state.editing = true
+    this.setState({
+      editing: true,
+      items: this.state.characters.map((item, itemIndex) => {
+        if (itemIndex === index) {
+          this.setState({
+            changedValue:value
+          })
+          // this.state.changedValue = value;
+          console.log("newchar", this.state.editing, item, value);
+          return {
+            ...item,
+            // editing: !item.editing
+          }
+        }
+        return item;
+      })
+    });
+  }
+
+
+
+  handlesubmit =  (character) => {
+    
+    console.log("this.state", this.state)
+    this.setState({
+      characters: [...this.state.characters, character],
+      editing:false,
+    });
+}
+
+
+      
+  handleupdate =  (updatedState) => {
+    
+    console.log("this.state in App", this.state, updatedState.username)
+    const index = this.state.characters.findIndex(p => p.username === this.state.changedValue)
+    console.log("Index at",index)
+    if(index!== -1){
+    
+      this.setState(prevState => {
+        if(updatedState.username!=='') 
+          this.state.characters[index].username = updatedState.username;
+        if(updatedState.email !== '')
+        this.state.characters[index].email = updatedState.email;
+      });
+
+      this.setState({
+        editing:false,
+      })
+    }
+    
+}
+
+  render() {
+    
+    /*Using state */
+    var uname = 'DemoUser'
+    const { characters } = this.state
+    characters.map((row) => {
+      uname = row.username
+    })
+
+    return (
+      
+
+      <Router>
+      <div>
+        <ul>
+          <li>
+            <NavLink to="/about"  activeStyle={{ color:'blue' }}>Registration</NavLink>
+          </li>
+          <li>
+            <NavLink to="/viewusers/user1" activeStyle={{color:'blue'}}>Users</NavLink>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route path="/about">
+            <Form handleupdate={this.handleupdate} handlesubmit={this.handlesubmit} editing={this.state.editing} changedValue={this.state.changedValue}/>
+          </Route>
+          <Route path='/viewusers/${uname}'>
+          <h1>Hello {uname}, Welcome to react JS1!</h1>
+            <Table characterSet={characters} removeData={this.removedata} editData={this.editdata} /> 
+          </Route>
+         
+        </Switch>
+      </div>
+    </Router>
+     
+      // console.log("user is:", uname),
+      // <div className="App container">
+      //   <h1>Hello {uname}, Welcome to react JS1!</h1>
+      //  {/* <Table characterSet = {characters} />   Passing variable to Table component*/}
+      
+      //  <Form handleupdate={this.handleupdate} handlesubmit={this.handlesubmit} editing={this.state.editing} changedValue={this.state.changedValue}/>
+       
+      
+      //  <Table characterSet={characters} removeData={this.removedata} editData={this.editdata} /> 
+
+      // </div>
+    )
+  }
+}
+
+export default App
---
+++ exampledemo/.history/src/Table_20200103100403.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+
+
+const TableHeader  = () => {
+    return(
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Password</th>
+        </tr>
+    </thead>
+    )
+}
+
+class Table extends Component {
+
+  render() {
+     
+
+    const {characterSet, removeData, editData} = this.props;  //Receives properties or data sent by parent component
+     console.log("+++++", characterSet)
+    const TableBody = props => {
+    const rows = props.characterData.map((row,index) => {   //map creates new array by executing the given function on every element of old array 
+    return(
+        
+       
+        <tr key={index}>
+            <td>{row.username}</td>
+            <td>{row.email}</td>
+            <td>{row.password}</td>
+            <td>
+                <button onClick={() => props.removedata(index)}>Delete</button>
+            </td> 
+            <td>
+                <button onClick={() => props.editData(index, row.username)}>Edit</button>
+                {/* <button onClick={() => props.editData(index, row)}>Edit</button> */}
+            </td>
+        </tr>
+       
+    )})
+
+     return <tbody>{rows}</tbody>
+}
+
+    return ( console.log(this.props)
+       
+        <div>
+            
+            <h1>Hello {this.props.username}, Welcome to react JS1!</h1>
+            <table>
+            <TableHeader />
+            <TableBody characterData={characterSet} removedata={removeData} editData={editData}/>
+            </table>
+        </div>
+    )
+    
+  }
+}
+
+export default Table
